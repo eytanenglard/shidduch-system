238fb70b037fe33526d4fc8a00b9a890dcdf8ec9
eytan englard <eytanenglard@gmail.com>
2025-04-06 18:30:14 +0300

    Your commit message

--- src/app/api/suggestions/[id]/inquiries/route.ts
+++ src/app/api/suggestions/[id]/inquiries/route.ts
@@ -3,8 +3,8 @@ import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
 import prisma from "@/lib/prisma";
-import { EmailService } from "@/app/components/matchmaker/suggestions/services/email/EmailService";
-
+/* import { EmailService } from "@/app/components/matchmaker/suggestions/services/email/EmailService";
+ */
 export async function POST(
   req: Request,
   { params }: { params: { id: string } }
@@ -82,11 +82,11 @@ const inquiry = await prisma.suggestionInquiry.create({
       `,
     };
 
-    await EmailService.getInstance().sendEmail({
+/*     await EmailService.getInstance().sendEmail({
       to: suggestion.matchmaker.email,
       subject: emailTemplate.subject,
       html: emailTemplate.body,
-    });
+    }); */
 
     return NextResponse.json({
       success: true,
