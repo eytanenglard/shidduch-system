abcf7ddd3186546960d5d92deb7522d5c6b48631
eytan englard <eytanenglard@gmail.com>
2025-04-20 09:39:58 +0300

    Your commit message

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,26 +1,9 @@
 import RegisterSteps from "@/app/components/auth/RegisterSteps";
-import Link from "next/link";
 
 export default function RegisterPage() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-      <div className="max-w-md w-full space-y-8">
-        <div>
-          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-            הרשמה למערכת
-          </h2>
-          <p className="mt-2 text-center text-sm text-gray-600">
-            או{" "}
-            <Link
-              href="/auth/signin"
-              className="font-medium text-indigo-600 hover:text-indigo-500"
-            >
-              התחברות למערכת
-            </Link>
-          </p>
-        </div>
+  
         <RegisterSteps />
-      </div>
-    </div>
+
   );
 }
